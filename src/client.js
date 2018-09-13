da359b09cb52d3005e385ae78d029aba516e2731
Alex <chess2ryme@gmail.com>
2018-09-13 15:47:13 +1000

    Modified to allow different socket URL

--- src/client.js
+++ src/client.js
@@ -2,10 +2,12 @@ const feathers = require('@feathersjs/feathers');
 const socketio = require('@feathersjs/socketio-client');
 const io = require('socket.io-client');
 
-const socket = io('http://ari.local:3030');
 const client = feathers();
 
 // Set up Socket.io client with the socket
-client.configure(socketio(socket));
+client.setup = (ip) => {
+	const socket = io(ip);
+	client.configure(socketio(socket));
+}
 
 export default client
